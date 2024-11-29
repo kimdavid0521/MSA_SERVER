8e5d24a22fd651f264cd4900b3f3cf58a56bedc4
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-11-29 23:19:05 +0900

    feat: ServiceDiscovery server 구현(eureka 서버 구현)

--- ServiceDiscovery/src/main/java/com/example/demo/ServiceDiscoveryApplication.java
+++ ServiceDiscovery/src/main/java/com/example/demo/ServiceDiscoveryApplication.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer  // EurekaServer 활성화
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
